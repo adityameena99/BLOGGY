547cd684ddb130d3e3314fbc42513341f5599506
Aditya Meena <130398523+adityameena99@users.noreply.github.com>
2025-11-06 15:52:35 +0530

    Update ForgotPasswordScreen.js

--- Frontend/src/components/AuthScreens/ForgotPasswordScreen.js
+++ Frontend/src/components/AuthScreens/ForgotPasswordScreen.js
@@ -1,62 +1,46 @@
 import { useState } from "react";
 import axios from "axios";
-
 import { Link } from "react-router-dom";
 import "../../Css/ForgotPassword.css"
 import { BsArrowBarLeft } from 'react-icons/bs'
+
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'https://bloggy-e52g.onrender.com'
+});
+
 const ForgotPasswordScreen = () => {
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
 
-
-  const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://bloggy-e52g.onrender.com'
-});
-
   const forgotPasswordHandler = async (e) => {
     e.preventDefault();
-
     try {
-      const { data } = await API.post(
-        "/auth/forgotpassword",
-        { email }
-      );
-
+      const { data } = await API.post("/auth/forgotpassword", { email });
       setSuccess(data.message);
     } catch (error) {
       setError(error.response.data.error);
       setEmail("");
-      setTimeout(() => {
-        setError("");
-      }, 5000);
+      setTimeout(() => setError(""), 5000);
     }
   };
 
   return (
     <div className="Inclusive-forgotPassword-page">
-
       <div className="forgotPassword-big-wrapper">
         <Link to="/" className="back_home">
           <BsArrowBarLeft />
         </Link>
-        <form
-          onSubmit={forgotPasswordHandler}
-        >
+        <form onSubmit={forgotPasswordHandler}>
           <div className="top-forgotpassword-explain">
-            <h3 >Forgot Password</h3>
-            <p >
-              Please enter the email address you register your account with. We
-              will send you reset password confirmation to this email
-            </p>
+            <h3>Forgot Password</h3>
+            <p>Please enter the email address you registered your account with. We will send you reset password confirmation to this email</p>
           </div>
 
           {error && <div className="error_message">{error}</div>}
-          {success && <div className="success_message  ">{success}  -
-            <Link to="/" className="ml-3">Go home</Link></div>}
+          {success && <div className="success_message">{success} - <Link to="/">Go home</Link></div>}
 
           <div className="input-wrapper">
-
             <input
               type="email"
               required
@@ -66,19 +50,12 @@ const ForgotPasswordScreen = () => {
               onChange={(e) => setEmail(e.target.value)}
             />
             <label htmlFor="email">E-mail</label>
-
           </div>
 
-          <button type="submit">
-            Send Email
-          </button>
-
+          <button type="submit">Send Email</button>
         </form>
-
       </div>
-
     </div>
-
   );
 };
 
